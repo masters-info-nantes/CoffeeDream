d7803f063265f3fcf7f9254b74e5836cedb4b14f
jbardon <bardonjeremy1@gmail.com>
2015-12-03 21:01:18 +0100

    Prepare code for mapdb

--- src/main/java/org/alma/middleware/coffeedream/AuthService.java
+++ src/main/java/org/alma/middleware/coffeedream/AuthService.java
@@ -6,53 +6,55 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
+import java.util.UUID;
 
 
 @Path("")
 public class AuthService {
 
-
-
-	@GET
-	@Path("/test/{name}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public void test(@PathParam("name") String name){
-        System.out.println("coucou");
-    }
-
 	@POST
 	@Path("/auth")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
+	public Response auth(HashMap<String, Object> request){
 
 
-	public Response auth(HashMap<String, Object> authJson){
-
-
-        int test = (Integer)authJson.get("test");
-
-        HashMap<String, Object> data = new HashMap<>();
+		HashMap<String, Object> data = new HashMap<>();
 		ResponseAuth resp = null;
 
-		switch (test){
+		// 1 - No IMEI in request
+		if(!request.containsKey("imei")){
+			data.put("error", "Please provide an imei number to get a token");
+			resp = new ResponseAuth("auth", data);
+			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		}
 
-			case 0:
-				data.put("error", "Token not found for this IMEI");
-				resp = new ResponseAuth("auth", data);
-				return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		// 2 - Bad IMEI formatting
+		String imeiNumber = (String) request.get("imei");
 
-			case 1:
-				data.put("error", "IMEI MAL FORMED");
-				resp = new ResponseAuth("auth", data);
-				return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		if(imeiNumber.length() != 15){
+			data.put("error", "Imei (" + imeiNumber + ") malformed. It contains " + imeiNumber.length() + " characters instead of 15.");
+			resp = new ResponseAuth("auth", data);
+			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		}
 
+		// 3 - IMEI found in user database
+		if(mapdb.contains(imeiNumber)){
+			String token = UUID.randomUUID().toString();
+			mapdb.add(token, imeiNumber);
 
-            default:
-                data.put("token", "KUGFYTDLUHKJFIIOM");
-                data.put("callback", "http://localhost:8080/coffeedream/token");
+			data.put("token", token);
+			data.put("callback", "http://localhost:8080/coffeedream/token");
 
-                resp = new ResponseAuth("auth", data);
-                return Response.ok(resp, MediaType.APPLICATION_JSON).build();
+			resp = new ResponseAuth("auth", data);
+			return Response.ok(resp, MediaType.APPLICATION_JSON).build();
+		}
+
+		// 4 - IMEI not found in user database
+		else {
+			data.put("error", "Imei (" + imeiNumber + ") not found in user database");
+			resp = new ResponseAuth("auth", data);
+			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
 		}
 	}
 
@@ -60,26 +62,46 @@ public class AuthService {
 	@Path("/token")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response token(@PathParam("token") String token, @PathParam("test") int test){
+	public Response token(HashMap<String, Object> request){
 
 		HashMap<String, Object> data = new HashMap<>();
 		ResponseAuth resp = null;
 
-		switch (test){
-			default:
-				data.put("user", new UserBean(689876874 , "John", "Doe"));
-				resp = new ResponseAuth("token", data);
-				return Response.ok(resp, MediaType.APPLICATION_JSON).build();
+		// 1 - No Token in request
+		if(!request.containsKey("token")){
+			data.put("error", "Please provide a token to get a user profile");
 
-			case 0:
-				data.put("error", "Token not found");
-				resp = new ResponseAuth("token", data);
-				return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			resp = new ResponseAuth("token", data);
+			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		}
 
-			case 1:
-				data.put("error", "Token not found for this IMEI");
-				resp = new ResponseAuth("token", data);
-				return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		// 2 - Bad token formatting
+		String tokenNumber = (String) request.get("token");
+
+		if(tokenNumber.length() != UUID.randomUUID().toString().length()){
+			data.put("error", "Token (" + tokenNumber + ") malformed.");
+
+			resp = new ResponseAuth("token", data);
+			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+		}
+
+		// 3 - Token found in database
+		if(mapdb.contains(tokenNumber)){
+			String imeiNumber = mapdb.get(tokenNumber);
+			UserBean user = mapdb.get(imeiNumber);
+
+			data.put("user", user);
+
+			resp = new ResponseAuth("token", data);
+			return Response.ok(resp, MediaType.APPLICATION_JSON).build();
+		}
+
+		// 4 - Token not found in database
+		else {
+			data.put("error", "Token (" + tokenNumber + ") not from this server");
+
+			resp = new ResponseAuth("token", data);
+			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
 		}
 	}
 }
