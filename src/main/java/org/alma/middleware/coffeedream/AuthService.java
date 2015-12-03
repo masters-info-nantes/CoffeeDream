717d0ae06e8f59efc3b83da84d2e41bc3296c711
jbardon <bardonjeremy1@gmail.com>
2015-12-03 21:18:28 +0100

    Include mapdb and better error handling

--- src/main/java/org/alma/middleware/coffeedream/AuthService.java
+++ src/main/java/org/alma/middleware/coffeedream/AuthService.java
@@ -18,43 +18,36 @@ public class AuthService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response auth(HashMap<String, Object> request){
 
-
-		HashMap<String, Object> data = new HashMap<>();
-		ResponseAuth resp = null;
-
 		// 1 - No IMEI in request
 		if(!request.containsKey("imei")){
-			data.put("error", "Please provide an imei number to get a token");
-			resp = new ResponseAuth("auth", data);
-			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			return makeError("auth", "Please provide an imei number to get a token");
 		}
 
 		// 2 - Bad IMEI formatting
 		String imeiNumber = (String) request.get("imei");
 
 		if(imeiNumber.length() != 15){
-			data.put("error", "Imei (" + imeiNumber + ") malformed. It contains " + imeiNumber.length() + " characters instead of 15.");
-			resp = new ResponseAuth("auth", data);
-			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			return makeError("auth", "Imei (" + imeiNumber + ") malformed. It contains " + imeiNumber.length() + " characters instead of 15.");
 		}
 
 		// 3 - IMEI found in user database
-		if(mapdb.contains(imeiNumber)){
-			String token = UUID.randomUUID().toString();
-			mapdb.add(token, imeiNumber);
+		MapDBStorage mapdb = new MapDBStorage();
 
+		if(mapdb.containsImei(imeiNumber)){
+			String token = UUID.randomUUID().toString();
+			mapdb.putToken(token, imeiNumber);
+
+			HashMap<String, Object> data = new HashMap<>();
 			data.put("token", token);
 			data.put("callback", "http://localhost:8080/coffeedream/token");
 
-			resp = new ResponseAuth("auth", data);
+			ResponseAuth resp = new ResponseAuth("auth", data);
 			return Response.ok(resp, MediaType.APPLICATION_JSON).build();
 		}
 
 		// 4 - IMEI not found in user database
 		else {
-			data.put("error", "Imei (" + imeiNumber + ") not found in user database");
-			resp = new ResponseAuth("auth", data);
-			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			return makeError("auth", "Imei (" + imeiNumber + ") not found in user database");
 		}
 	}
 
@@ -64,44 +57,42 @@ public class AuthService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response token(HashMap<String, Object> request){
 
-		HashMap<String, Object> data = new HashMap<>();
-		ResponseAuth resp = null;
-
 		// 1 - No Token in request
 		if(!request.containsKey("token")){
-			data.put("error", "Please provide a token to get a user profile");
-
-			resp = new ResponseAuth("token", data);
-			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			return makeError("token", "Please provide a token to get a user profile.");
 		}
 
 		// 2 - Bad token formatting
 		String tokenNumber = (String) request.get("token");
 
 		if(tokenNumber.length() != UUID.randomUUID().toString().length()){
-			data.put("error", "Token (" + tokenNumber + ") malformed.");
-
-			resp = new ResponseAuth("token", data);
-			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			return makeError("token", "Token (" + tokenNumber + ") malformed.");
 		}
 
 		// 3 - Token found in database
-		if(mapdb.contains(tokenNumber)){
-			String imeiNumber = mapdb.get(tokenNumber);
-			UserBean user = mapdb.get(imeiNumber);
+		MapDBStorage mapdb = new MapDBStorage();
 
+		if(mapdb.containsToken(tokenNumber)){
+			UserBean user = mapdb.getUser(tokenNumber);
+
+			HashMap<String, Object> data = new HashMap<>();
 			data.put("user", user);
 
-			resp = new ResponseAuth("token", data);
+			ResponseAuth resp = new ResponseAuth("token", data);
 			return Response.ok(resp, MediaType.APPLICATION_JSON).build();
 		}
 
 		// 4 - Token not found in database
 		else {
-			data.put("error", "Token (" + tokenNumber + ") not from this server");
-
-			resp = new ResponseAuth("token", data);
-			return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
+			return makeError("token", "Token (" + tokenNumber + ") not from this server.");
 		}
+	}
+
+	private Response makeError(String caller, String message){
+		HashMap<String, Object> data = new HashMap<>();
+		data.put("error", message);
+
+		ResponseAuth resp = new ResponseAuth(caller, data);
+		return Response.status(Response.Status.NOT_FOUND).entity(resp).build();
 	}
 }
